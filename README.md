06e953a7dea00de3b940e45324cdbbc9b1acb704
Andy963 <zjgxs518@126.com>
2022-04-04 19:01:48 +0800

    update: basic function

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# Video Downloader Telegram Bot (Youtube Downloader)
-This bot use youtube dl to download video from Stream urls which you can't download from for example Youtube Videos
+## ytd (Youtube Downloader)
+This bot use yt_dlp to download video from Stream urls which you can't download from for example Youtube Videos
 
 * this bot based on Pyrogram library and YoutubeDL
 
-# Install Requirements
+## Install Requirements
 Use this command :
 
     pip install -r requirements.txt
 or :
 
     pip3 install -r requirements.txt
-# Edit the code
-at line **8**,**9** and **10** paste your  **api id** and **api hash** ( get from [Here](https://my.telegram.org) ) and **bot token** ( get it from [BotFather](t.me/botfather) )
+## Edit the code
+edit config paste your  **api id** and **api hash** ( get from [Here](https://my.telegram.org) ) and **bot token** ( get it from [BotFather](t.me/botfather) )
 
 other Edit lines are Commented in The **main.py** Source :)
-# Run The Bot
+## Run The Bot
 
 After Install requirements and edit the code Run The bot Use this command or use your editor or etc,
 
@@ -29,3 +29,6 @@ The Bot Need ffmpeg so here is some link to know How install it on your OS :
 > Thanks to my Dear Friend [Hassan Hoot](https://github.com/hassanhoot) For The Progress Bar
 
 **I hope Enjoy The Bot :)**
+
+## TODO
+delete file after upload to tg
--- main.py
+++ main.py
@@ -1,145 +1,147 @@
 # -*- coding: utf-8 -*-
+from enum import Enum
 
 import requests, os, validators
-import youtube_dl
+import yt_dlp
 from pyrogram import Client, filters
-from pyrogram.types import  InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import api_id, api_hash, token, chunk_size, download_path
 
-api_id = 123456789 #int of api id get from my.telegram.org
-api_hash = " Your Api Hash Here " #str of api hash get from my.telegram.org
-token = ' Your Bot Token here ' #str of token get from BotFather
-app = Client("Downlaoder", api_id, api_hash, bot_token=token) # You Can Change The Session Name by Replace "Downlaoder" to your session name
+app = Client("Downloader", api_id, api_hash, bot_token=token, )
+# proxy=dict(hostname="127.0.0.1", port=51837)
+video_pattern = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
 
 
-def downloada(url, quality):
-    
-    if quality == "1":
+class Quality:
+    high = 1
+    normal = 2
+
+
+def download_file(url, quality):
+    quality = int(quality)
+    if quality == Quality.high:
         ydl_opts_start = {
-            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best', #This Method Need ffmpeg
-            'outtmpl': f'localhoct/%(id)s.%(ext)s',
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',  # This Method Need ffmpeg
+            'outtmpl': f'{download_path}/%(id)s.%(ext)s',
             'no_warnings': True,
             'ignoreerrors': True,
             'noplaylist': True,
-            'http_chunk_size': 20097152,
+            'http_chunk_size': chunk_size,
             'writethumbnail': True
 
         }
-        with youtube_dl.YoutubeDL(ydl_opts_start) as ydl:
+        with yt_dlp.YoutubeDL(ydl_opts_start) as ydl:
             result = ydl.extract_info("{}".format(url))
             title = ydl.prepare_filename(result)
             ydl.download([url])
         return title
-    
-    if quality == "2":
+
+    if quality == Quality.normal:
         ydl_opts_start = {
-            'format': 'best', #This Method Don't Need ffmpeg , if you don't have ffmpeg use This 
-            'outtmpl': f'localhoct/%(id)s.%(ext)s',
+            'format': 'best',  # This Method Don't Need ffmpeg , if you don't have ffmpeg use This
+            'outtmpl': f'{download_path}/%(id)s.%(ext)s',
             'no_warnings': False,
             'logtostderr': False,
             'ignoreerrors': False,
             'noplaylist': True,
-            'http_chunk_size': 2097152,
+            'http_chunk_size': chunk_size,
             'writethumbnail': True
         }
-        with youtube_dl.YoutubeDL(ydl_opts_start) as ydl:
-            result = ydl.extract_info("{}".format(url))
-            title = ydl.prepare_filename(result)
-            ydl.download([url])
-        return f'{title}'
-    
-    if quality == "3":
-        ydl_opts_start = {
-            'format': 'best[height=480]',
-            'outtmpl': f'localhoct/%(id)s.%(ext)s',
-            'no_warnings': False,
-            'logtostderr': False,
-            'ignoreerrors': False,
-            'noplaylist': True,
-            'http_chunk_size': 2097152,
-            'writethumbnail': True
-        }
-        with youtube_dl.YoutubeDL(ydl_opts_start) as ydl:
+        with yt_dlp.YoutubeDL(ydl_opts_start) as ydl:
             result = ydl.extract_info("{}".format(url))
             title = ydl.prepare_filename(result)
             ydl.download([url])
         return f'{title}'
 
-# here you can Edit Start message
+
 @app.on_message(filters.command('start', '/'))
-def start(c, m): # c Mean Client | m Mean Message
-    m.reply_text('Hi Welcome To @iLoaderBot \n Just Send Video Url To me and i\'ll try to upload the video and send it to you') #Edit it and add your Bot ID :)
+def start(_, message):
+    """
+    Func:start response
+    Args:message: message
+    Example:
+    Return: None
+    Author: Andy
+    Version: 1.0
+    Created: 2022/4/4 下午4:04
+    Modified: 2022/4/4 下午4:04
+    """
+    message.reply_text((
+        f"Welcome to ytd-dl!\n"
+        f"Just send Video url, and let me try to download.")
+    )
 
 
-@app.on_message(filters.regex(
-    r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"))
-def webpage(c, m): # c Mean Client | m Mean Message
-    url1 = m.text
-    if validators.url(url1):
-        sample_url = "https://da.gd/s?url={}".format(url1)
-        url = requests.get(sample_url).text
-        chat_id = m.chat.id
-        keys = c.send_message(
+@app.on_message(filters.regex(video_pattern))
+def webpage(client, message):
+    url = message.text
+    if validators.url(url):
+        sample_url = "https://da.gd/s?url={}".format(url)
+        new_url = requests.get(sample_url).text
+        chat_id = message.chat.id
+        keys = client.send_message(
             chat_id,
-            f"Okay!!🙄\n {url1} is Video Url😊 \n\nPlease Select Quality :\n💡The HD Button is Download the Best Quality is available so I recommend This Button😁 ",
+            (f"Good! {url} is a valid video url.\n"
+             f"now please select quality:\n"
+             ),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(
                             "HD (Recommended) - Need ffmpeg",
-                            callback_data="%s and 1" % url
+                            callback_data=f"{new_url}||{Quality.high}"
                         ),
                     ],
                     [
                         InlineKeyboardButton(
                             "HD - Don't Need ffmpeg",
-                            callback_data="%s and 2" % url
-                        ),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                            "SD (480) Maybe Don't Work",
-                            callback_data= "%s and 3" % url
+                            callback_data=f"{new_url}||{Quality.normal}"
                         ),
                     ]
                 ]
             ), disable_web_page_preview=True
         )
     else:
-        c.send_message(m.chat.id,"Send The Valid Url Please")
+        client.send_message(message.chat.id, "Send The Valid Url Please")
 
 
 @app.on_callback_query()
-def download(c, q): # c Mean Client | q Mean Query
+def download(client, query):  # c Mean Client | q Mean Query
     global check_current
     check_current = 0
-    def progress(current, total): #Thanks to my dear friend Hassan Hoot for Progress Bar :)
+
+    def progress(current, total):  # Thanks to my dear friend Hassan Hoot for Progress Bar :)
         global check_current
-        if ((current//1024//1024) % 50 )== 0 :
-            if check_current != (current//1024//1024):
-                check_current = (current//1024//1024)
-                upmsg.edit(f"{current//1024//1024}MB of {total//1024//1024}MB Uploaded 😁")
-        elif (current//1024//1024) == (total//1024//1024):
-            upmsg.delete()
-    
-    chat_id = q.message.chat.id
-    data = q.data
-    url, quaitly = data.split(" and ")
-    dlmsg = c.send_message(chat_id, 'Hmm!😋 Downloading...')
-    path = downloada(url, quaitly)
-    upmsg = c.send_message(chat_id, 'Yeah😁 Uploading...')
-    dlmsg.delete()
-    thumb = path.replace('.mp4',".jpg",-1)
-    if  os.path.isfile(thumb):
-        thumb = open(thumb,"rb")
+        if ((current // 1024 // 1024) % 50) == 0:
+            if check_current != (current // 1024 // 1024):
+                check_current = (current // 1024 // 1024)
+                upload_msg.edit(f"{current // 1024 // 1024}MB of {total // 1024 // 1024}MB Uploaded 😁")
+        elif (current // 1024 // 1024) == (total // 1024 // 1024):
+            upload_msg.delete()
+
+    chat_id = query.message.chat.id
+    data = query.data
+    url, quality = data.split("||")
+    download_msg = client.send_message(chat_id, 'Hmm!😋 Downloading...')
+    path = download_file(url, quality)
+    upload_msg = client.send_message(chat_id, 'Yeah😁 Uploading...')
+    download_msg.delete()
+    thumb = path.replace('.mp4', ".jpg", -1)
+    if os.path.isfile(thumb):
+        thumb = open(thumb, "rb")
         path = open(path, 'rb')
-        c.send_photo(chat_id,thumb,caption='Thumbnail of the video Downloaded by @iLoaderBot') #Edit it and add your Bot ID :)
-        c.send_video(chat_id, path, caption='Downloaded by @iLoaderBot',
-                    file_name="iLoader", supports_streaming=True, progress=progress) #Edit it and add your Bot ID :)
-        upmsg.delete()
+        client.send_photo(chat_id, thumb,
+                          caption='Thumbnail of the video Downloaded')  # Edit it and add your Bot ID :)
+        client.send_video(chat_id, path, caption='Downloaded',
+                          file_name="andy", supports_streaming=True,
+                          progress=progress)  # Edit it and add your Bot ID :)
+        upload_msg.delete()
     else:
         path = open(path, 'rb')
-        c.send_video(chat_id, path, caption='Downloaded by @iLoaderBot',
-                    file_name="iLoader", supports_streaming=True, progress=progress)
-        upmsg.delete()
+        client.send_video(chat_id, path, caption='Downloaded by @yinshan_bot',
+                          file_name="iLoader", supports_streaming=True, progress=progress)
+        upload_msg.delete()
 
-app.run()
+
+if __name__ == '__main__':
+    app.run()
